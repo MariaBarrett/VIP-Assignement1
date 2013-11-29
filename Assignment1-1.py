bf217941265d5946876b7e30f0d3e1b9e6c9781a
MariaBarrett <Maria@d048.edur.sc.ku.dk>
2013-11-29 18:31:39 +0100

    alternative functions
    I added simpler functions as alternatives. Just comment out the
    function you don’t want

--- Assignment1-1.py
+++ Assignment1-1.py
@@ -4,6 +4,8 @@ from scipy.ndimage import filters
 from numpy import *
 import matplotlib.pyplot as plt
 import math
+from scipy.spatial.distance import euclidean
+
 
 plt.gray()
 # Import images and tranfer them into 2D arrays
@@ -33,14 +35,14 @@ def detectinterest(image_GF):
   return interest
 
 
-
-def patch(image):
+def patch(image,width):
+    wid = int(width / 2)
     patch=[]
     Patch=[]
     interest=detectinterest(image)
     for point in interest:
-       for i in range(point[0]-1,point[0]+2):
-           for j in range(point[1]-1,point[1]+2):
+       for i in range(point[0]-wid,point[0]+wid+1):
+           for j in range(point[1]-wid,point[1]+wid+1):
                patch.append(image[i,j])
        
        patch.append(point[0])
@@ -49,8 +51,8 @@ def patch(image):
        patch=[]
     
     return Patch
-    
-def compute(Patch,image):#here patch looks like [value, value, value... x, y]
+"""    
+def compute(Patch):#here patch looks like [value, value, value... x, y]
     sum=0
     sum2=0
     for i in range(len(Patch)-2):
@@ -59,21 +61,28 @@ def compute(Patch,image):#here patch looks like [value, value, value... x, y]
     
     for j in range(len(Patch)-2):
         sum2+=(Patch[j]-meanvalue)**2    
-    standard=sum2/(len(Patch)-2)
+    standard=sqrt(sum2/(len(Patch)-2))
     
     return meanvalue,standard
-
+"""
 def NCC(patch1,patch2):
-    mean1,sta1=compute(patch1,image_GF)
-    mean2,sta2=compute(patch2,image_GF2)
+#A function that takes two patches and returns the
+#normalized cross correlation value for every match
+    mean1,sta1= mean(patch1[:9]), std(patch1[:9]) # the first 9 values are the gray values. The last two are coordinates of the center
+    mean2,sta2= mean(patch2[:9]), std(patch2[:9])
+
+    #mean1,sta1= compute(patch1)
+    #mean2,sta2= compute(patch2)    
     sum=0
     for i in range(len(patch1)-2):
-         sum+=((patch1[i]-mean1)*(patch2[i]-mean2))/(sta1*sta2)
-         
-    NCC=1-(sum/(len(patch1)-2))    
+         sum+=((patch1[i]-mean1)*(patch2[i]-mean2))/(sta1*sta2)        
+    NCC=(sum/(len(patch1)-2))  
     return NCC
-    
-def evaluate(Match1,Match2):#here match1 is patch and match2 is something like [patch2, patch2, patch2]
+"""  
+def evaluate(Match1,Match2):
+#Euclidean distance. Match 1 is one patch. Match 2 is a list of possible matches. 
+#if the distance from the best candidate from Match2 is more than 0.8 of 
+#evaluate using bubble sort
     sum=0
     Distance=[]
     for match2 in Match2:
@@ -85,18 +94,40 @@ def evaluate(Match1,Match2):#here match1 is patch and match2 is something like [
     
     for key in range(len(Match2)-1):
        for index in range(len(Match2)-key-1):
-          if  Distance[index]>Distance[index+1]:
+          if  Distance[index]>Distance[index+1]: 
                Distance[index],Distance[index+1]=Distance[index+1],Distance[index]
                Match2[index],Match2[index+1]=Match2[index+1],Match2[index]
     
     if Distance[0]/Distance[1]<=0.8:
         return Match2[0]
     else: return []
-    
+"""
+def evaluate(Match1,Match2):
+#Evaluate matches using euclidean distance. Match 1 is one patch. Match 2 is a list of possible matches. 
+#if the distance from the best candidate from Match2 is more than 0.8 of 
+    sum=0
+    Distance=[]
+    for match2 in Match2:
+        distance = euclidean(Match1, match2)
+        Distance.append(distance)
+        sum = 0        
+    Distance = sorted(Distance)
+    if Distance[0]/Distance[1]<=0.8:
+        return Match2[0]
+    else: return []
+
+ 
 def match(patchlist1,patchlist2,threshold):# Question: Is it possible two points in image1 match the same point in image2
-    Matched=[]
-    Matched1=[]
-    Matched2=[]
+    #This function matches one patch from one image to all patches in the other image
+    # calling the NCC function. If the ncc value is above the threshold
+    #patches from each list is stored in Matched1 and Matched2 with the same index number
+    #If the best match from Match2 is more than 0.8 closer than the second best, this function returns stores the two matches in 
+    #the lists best_matched1 and best_matched2
+    Matched=[] #temporary list
+    Matched1=[] # a 1D list of patches from patchlist1 that has matches above the threshold from list 2
+    Matched2=[] # a list of list of (more) patches from patchlist 2 that matches one patch list1
+    best_matched1=[] # a list of best matches from image1
+    best_matched2=[] # a list of best matches from image2
     for patch1 in patchlist1:
         for patch2 in patchlist2:                   
             if NCC(patch1,patch2)>threshold:
@@ -104,27 +135,23 @@ def match(patchlist1,patchlist2,threshold):# Question: Is it possible two points
         if Matched!=[]:
             Matched2.append(Matched)
             Matched1.append(patch1)
-        Matched=[]
-        
-    # evaluate if the mathed interests are good matched interests        
-    real_matched1=[]
-    real_matched2=[]
-    for index in range(len(Matched1)):
-        if len(Matched2[index])==1:
-            real_matched1.append([Matched1[index][9],Matched1[index][10]])            
-            real_matched2.append([Matched2[index][0][9],Matched2[index][0][10]])
+        Matched=[]        
+    # evaluate if the best match is convincing
+    for i in range(len(Matched1)):
+        if len(Matched2[i])==1: # if there is only one candidate
+            best_matched1.append([Matched1[i][9],Matched1[i][10]]) #append coordinates           
+            best_matched2.append([Matched2[i][0][9],Matched2[i][0][10]]) # append coordinates
             
-        elif len(Matched2[index])>1:
-                 evaluation=evaluate(Matched1[index],Matched2[index])
-                 if evaluation!=[]: 
-                     real_matched1.append([Matched1[index][9],Matched1[index][10]])
-                     real_matched2.append([evaluation[9],evaluation[10]])
-            
-                     
-    return real_matched1,real_matched2
+        elif len(Matched2[i])>1: # if more than one candidate: see if the best in convincing
+            evaluation=evaluate(Matched1[i],Matched2[i])
+            if evaluation!=[]: 
+                best_matched1.append([Matched1[i][9],Matched1[i][10]])
+                best_matched2.append([evaluation[9],evaluation[10]])
+                               
+    return best_matched1,best_matched2
 
-patchlist1=patch(image_GF)
-patchlist2=patch(image_GF2)
+patchlist1=patch(image_GF, 3)
+patchlist2=patch(image_GF2, 3)
 
 Points1,Points2=match(patchlist1,patchlist2,threshold=0.5)
 
@@ -134,7 +161,7 @@ def plot_matches(image1,image2,match1,match2):
     image3 = concatenate((image1, image2), axis=1)
     plt.imshow(image3)
     cols = 800
-    #cols = im1.shape[1]#800
+    #cols = image1.shape[1]#800
     p = 0
     assert len(match1) == len(match2)
     for i in range (len(match1)):
@@ -155,4 +182,6 @@ plt.imshow(image2, origin='lower')
 plt.plot([p[1] for p in Points2], [p[0] for p in Points2], 'r.')
 plt.axis('off')
 plt.show()
+
+
 
